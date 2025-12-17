32d86835de2cef8f7d15dccf8518a61a16159282
hrt05 <hirate05program@gmail.com>
2025-12-17 21:39:01 +0900

    first commit

---
+++ sinasai.js
@@ -0,0 +1,14 @@
+(() => {
+    const noOpenPage = [
+        "x.com",
+        "www.youtube.com"
+    ];
+
+    //現在開いているところを変数に挿入
+    const nowPage = window.location.hostname;
+
+    // もし現在開かれてるページがnoOpenPageに含まれていたら
+    if (noOpenPage.includes(nowPage)) {
+        window.location.href = "https://www.google.com";
+    }
+})();
